73b2d48b74b43ac4a5598792c8dd3215783d24d8
Rhyan de Oliveira Fischer <85214488+RhyanFischer@users.noreply.github.com>
2022-05-07 00:50:12 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exerc1_Tabuada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i;
+            int t = int.Parse(textBox1.Text);         
+            for (i = 0; i <= 10; i++)
+            {
+                listBox1.Items.Add(t + " * " + i + " = " +t*i);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ;
+        }
+    }
+}
